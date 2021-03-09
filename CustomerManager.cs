58093ee24c98fa3e705d48c7791f9e5eb86e19f4
yesimkeles <53233077+yesimkeles@users.noreply.github.com>
2021-03-09 23:47:23 +0300

    ClassMetotDemo

---
+++ CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer musteri)
+        {
+            Console.WriteLine(musteri.Id +" "+ musteri.Adi +" "+ musteri.Soyadi +" "+ musteri.TCKimlikNo +" "+ musteri.Dogumtarih);
+            Console.WriteLine("Ekleme işlemi yapıldı.");
+        }
+
+        public void CustomerList(Customer [] musteri)
+        {
+            Console.WriteLine("Array List");
+            foreach (var item in musteri)
+            {
+                Console.WriteLine(item.Id + " " + item.Adi + " " + item.Soyadi + " " + item.TCKimlikNo + " " + item.Dogumtarih);
+            }
+        }
+
+        public void CustomerRemove(Customer musteri)
+        {
+            Console.WriteLine("Silme işlemi yapıldı." + musteri.Adi);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer Musteri = new Customer();
+            Musteri.Id = 1;
+            Musteri.Adi = "Yeşim";
+            Musteri.Soyadi = "KELEŞ";
+            Musteri.TCKimlikNo = "111111111111";
+            Musteri.Dogumtarih = "10.01.1990";
+
+            Customer Musteri2 = new Customer();
+            Musteri2.Id = 2;
+            Musteri2.Adi = "Betül";
+            Musteri2.Soyadi = "CAN";
+            Musteri2.TCKimlikNo = "222222222222";
+            Musteri2.Dogumtarih = "14.06.1996";
+
+            Customer Musteri3 = new Customer();
+            Musteri3.Id = 3;
+            Musteri3.Adi = "Ali";
+            Musteri3.Soyadi = "KARA";
+            Musteri3.TCKimlikNo = "5555555555555";
+            Musteri3.Dogumtarih = "25.10.1988";
+
+            Customer[] musteriler = new Customer[] { Musteri, Musteri2, Musteri3 };
+
+            CustomerManager MusteriYonetimi = new CustomerManager();
+            MusteriYonetimi.Add(Musteri);
+            MusteriYonetimi.Add(Musteri2);
+            MusteriYonetimi.Add(Musteri3);
+
+            Console.WriteLine("--------------------------------");
+            MusteriYonetimi.CustomerList(musteriler);
+
+            Console.WriteLine("--------------------------------");
+
+            MusteriYonetimi.CustomerRemove(Musteri3);
+            
+
+
+        }
+    }
+}
